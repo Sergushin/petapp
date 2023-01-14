c0debe859c07c94be6dddeb977654a7ad5acb49f
Benjamin <sergushinbenjamin@gmail.com>
2023-01-14 10:06:44 +0300

    Add CharkaUI and theme.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript } from '@chakra-ui/react'
+import theme from '@/components/theme'
+
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        {/* 👇 Here's the script */}
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
