9224d5dcdfee756fab7d00e94cccddffdcdccd54
Benjamin <112263801+Sergushin@users.noreply.github.com>
2023-01-24 00:23:55 +0300

    without next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-const { default: absoluteUrl } = require('next-absolute-url')
-const {origin}  = absoluteUrl()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${origin.protocol}/${origin.host}/:path*`,
-      },
-    ]
-  },
+  
 }
 
 module.exports = nextConfig
