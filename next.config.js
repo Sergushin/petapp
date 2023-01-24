4ab5069d75c03748e31df925423006de8a8aeddd
Benjamin <112263801+Sergushin@users.noreply.github.com>
2023-01-24 11:14:53 +0300

    chage next js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
+if (typeof window !== "undefined") {
+  var host = window.location.host;     
+}
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `https://${host}/:path*`,
+      },
+    ]
+  },
   
 }
 
