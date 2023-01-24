bae50b93fb289e52a39535c9ff16ce1a289b1e62
Benjamin <112263801+Sergushin@users.noreply.github.com>
2023-01-24 13:55:16 +0300

    next js fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  reactStrictMode: true,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: 'src/api/:path*',
+  //       destination: `https://benjamin-petapp.vercel.app/:path*`,
+  //     },
+  //   ]
+  // },
   
 }
 
