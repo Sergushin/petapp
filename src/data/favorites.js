31d3397d2c51ac91e474b6fc85a43e0325b4fa56
Benjamin <112263801+Sergushin@users.noreply.github.com>
2023-01-28 22:14:04 +0300

    some fix

--- src/data/favorites.js
+++ src/data/favorites.js
@@ -1,19 +1,4 @@
 
 export const favorites = [
-    {
-        "id": 1,
-        "name": "Vaskas",
-        "author": "Sergey",
-        "type": "cats",
-        "owner": "Benjamin",
-        "about": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque distinctio consequatur molestiae, earum ducimus fugiat vel fuga nostrum excepturi nam ullam adipisci quas praesentium totam est iure sed voluptate atque."
-    },
-    {
-        "id": 2,
-        "name": "Liza",
-        "author": "Katya",
-        "type": "cats",
-        "owner": "Benjamin",
-        "about": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque distinctio consequatur molestiae, earum ducimus fugiat vel fuga nostrum excepturi nam ullam adipisci quas praesentium totam est iure sed voluptate atque."
-    },
+   
 ]
